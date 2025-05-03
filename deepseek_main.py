c8c28cf5bde294f87b48735de36d5cec10faf8fc
Stefanus Gusega Gunawan <gunawanstefanus006@gmail.com>
2025-05-03 18:01:13 +0700

    feat(playground): add deepseek scripts

---
+++ deepseek_main.py
@@ -0,0 +1,26 @@
+"""
+This is a simple example of how to use the DeepSeek API with the OpenAI Python client.
+"""
+
+from openai import OpenAI
+from icecream import ic
+
+API_KEY = "sk-c657027d398d411eb72e38964a399633"
+BASE_URL = "https://api.deepseek.com"
+
+client = OpenAI(api_key=API_KEY, base_url=BASE_URL)
+
+if __name__ == "__main__":
+    response = client.chat.completions.create(
+        model="deepseek-chat",
+        messages=[
+            {
+                "role": "system",
+                "content": "You are a helpful assistant that helps with programming tasks.",
+            },
+            {"role": "user", "content": "Provide me a simpe fake JSON file."},
+        ],
+        stream=False,
+    )
+
+    ic(response)
