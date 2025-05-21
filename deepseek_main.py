ce56491178faa90ac1d6a42adad46b7d8549957a
Stefanus Gusega Gunawan <gunawanstefanus006@gmail.com>
2025-05-21 23:57:33 +0700

    fix: use .env file to store DeepSeek API Key

--- deepseek_main.py
+++ deepseek_main.py
@@ -4,8 +4,12 @@ This is a simple example of how to use the DeepSeek API with the OpenAI Python c
 
 from openai import OpenAI
 from icecream import ic
+from dotenv import load_dotenv
+import os
 
-API_KEY = "sk-c657027d398d411eb72e38964a399633"
+load_dotenv()
+
+API_KEY = os.environ["DEEPSEEK_API_KEY"]
 BASE_URL = "https://api.deepseek.com"
 
 client = OpenAI(api_key=API_KEY, base_url=BASE_URL)
